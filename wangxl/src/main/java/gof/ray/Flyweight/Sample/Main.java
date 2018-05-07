0bab2d4c18812bb773935ac819d01ea628a62d86
k0363224 <32411043@qq.com>
2018-05-07 17:34:08 +0800

    321321

--- wangxl/src/main/java/gof/ray/Flyweight/Sample/Main.java
+++ wangxl/src/main/java/gof/ray/Flyweight/Sample/Main.java
@@ -3,7 +3,7 @@ package gof.ray.Flyweight.Sample;
 public class Main {
     public static void main(String[] args) {
         String s = "123456";
-        BigString bs = new BigString( s );
+        BigString bs = new BigString(s);
         bs.print();
     }
 }
