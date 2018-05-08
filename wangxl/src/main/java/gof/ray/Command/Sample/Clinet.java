a979273159f0e34aa6761d46f414626b235d6036
k0363224 <32411043@qq.com>
2018-05-08 16:19:35 +0800

    312312

--- wangxl/src/main/java/gof/ray/Command/Sample/Clinet.java
+++ wangxl/src/main/java/gof/ray/Command/Sample/Clinet.java
@@ -8,9 +8,12 @@ import gof.ray.Command.Sample.receiver.Receiver;
 
 public class Clinet {
     public static void main(String[] args) {
+        // 命令执行者
         Receiver receiver = new Receiver();
+        // 命令调用者
         Invoker invoker = new Invoker();
 
+        // 命令
         Command turnOnLight = new LightOnCommand(receiver);
         Command turnOffLight = new LightOffCommand(receiver);
 
