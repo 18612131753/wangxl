fffd53779bbdc14709af5c12aceb2e74d5fa342d
k0363224 <32411043@qq.com>
2018-05-08 14:40:44 +0800

    1212

---
+++ wangxl/src/main/java/gof/ray/Proxy/javaproxy/T.java
@@ -0,0 +1,10 @@
+package gof.ray.Proxy.javaproxy;
+
+public class T {
+
+    public static void main(String[] args) {
+        // org.springframework.aop.framework.JdkDynamicAopProxy  s = null;
+        // org.springframework.aop.framework.CglibAopProxy      s1 ;
+    }
+
+}
