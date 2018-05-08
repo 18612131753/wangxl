32311f7ab67c742f605b393e60d53731120af85a
k0363224 <32411043@qq.com>
2018-05-08 16:36:40 +0800

    32131

--- wangxl/src/main/java/gof/ray/Interpreter/Sample/Test.java
+++ wangxl/src/main/java/gof/ray/Interpreter/Sample/Test.java
@@ -7,15 +7,17 @@ public class Test {
         // 规则：Robert 和 John 是男性
         Expression robert = new TerminalExpression("Robert");
         Expression john = new TerminalExpression("John");
-        Expression isMale = new OrExpression(robert, john);
-        System.out.println("John is male? " + isMale.interpret("John"));
-        System.out.println("Robert is male? " + isMale.interpret("Robert"));
-        System.out.println("Has a male? " + isMale.interpret("Robert 1 Julie"));
+        Expression orMale = new OrExpression(robert, john);
         
+        System.out.println("John is male? " + orMale.interpret("John"));
+        System.out.println("Robert is male? " + orMale.interpret("Robert"));
+        System.out.println("Has a male? " + orMale.interpret("Robert 1 Julie"));
+
         // 规则：Julie 是一个已婚的女性
         Expression julie = new TerminalExpression("Julie");
         Expression married = new TerminalExpression("Married");
-        Expression isMarriedWoman = new AndExpression(julie, married);
-        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
+        Expression andMarriedWoman = new AndExpression(julie, married);
+        System.out.println("Julie is a married women? " + andMarriedWoman.interpret("Married Julie"));
+        System.out.println("John is a married women? " + andMarriedWoman.interpret("Married John"));
     }
 }
